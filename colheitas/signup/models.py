2e45aca8b027de639a5ca7a6a21cc62b21adf2dc
Isaque Lopes Campello <isaque.campello@gmail.com>
2022-03-22 23:21:47 -0300

    added preliminary model

--- colheitas/signup/models.py
+++ colheitas/signup/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Seller(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    state = models.CharField(max_length=20)
+    region = models.CharField(max_length=50)
+    singup_date = models.DateTimeField()
+    items_sold = models.CharField(max_length=1000)
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    state = models.CharField(max_length=20)
+    region = models.CharField(max_length=50)
+    singup_date = models.DateTimeField()
