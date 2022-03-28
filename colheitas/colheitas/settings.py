05d37365180f45bd0dd9aceada42789846bf5fff
Isaque Lopes Campello <isaque.campello@gmail.com>
2022-03-27 21:19:28 -0300

    updated gitignore and deleted migrations

--- colheitas/colheitas/settings.py
+++ colheitas/colheitas/settings.py
@@ -78,15 +78,8 @@ WSGI_APPLICATION = 'colheitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'colheitas_db',
-        'USER': 'django',
-        'PASSWORD': 'SoftEng2022',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-         'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
