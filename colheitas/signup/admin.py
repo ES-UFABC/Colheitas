df5e73a2dea70b5596f51970351978de0ef1d3bb
Isaque Lopes Campello <isaque.campello@gmail.com>
2022-04-04 19:42:51 -0300

    deleted signup and landing page projects, which are no longer used

--- colheitas/signup/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Seller, Seller_Contact, Certifications, Buyer
-
-# admin.site.register(Seller)
-# admin.site.register(Seller_Contact)
-# admin.site.register(Certifications)
