a055fa444aaff52ab5e1338cad4b90d8433d3582
Lribeiro27 <lucas.rcarneiro27@gmail.com>
2022-04-04 18:54:21 -0300

    updating delete files from crud

--- colheitas/products/views.py
+++ colheitas/products/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from multiprocessing import Event
+from django.shortcuts import redirect, render
+from numpy import product
+import pkg_resources
+
+from colheitas.accounts.models import Product
 from .forms import ProductForm
 
+def delete_product(request, id):
+    product_to_delete = Product.objects.get(id=id)
+    
+    if request.method == 'POST':
+        product_to_delete.delete()
+        return redirect('product_delete.html')
+    
+    return render(request, 'products/product_delete_confirmation.html', {'product': product})
+    #TODO Product_delete_confirmation, html simples para confirmar o delete
 
 def product_register(request):
     if request.method == 'POST':
