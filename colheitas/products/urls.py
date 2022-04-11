e10ad99ac02292379450d5d86a7f780a0a03a787
Lribeiro27 <lucas.rcarneiro27@gmail.com>
2022-04-11 11:42:02 -0300

    Updates in delete function

--- colheitas/products/urls.py
+++ colheitas/products/urls.py
@@ -5,9 +5,8 @@ from django.urls import include, path
 from .views import ProductRegisterView# product_register
 from .views import delete_product
 
-
 urlpatterns = [
     # path('register/', product_register, name='product_register'),
     path('register/', ProductRegisterView.as_view(), name='product_register'),
-    path('delete_product/<id>', delete_product, name = 'delete_product')
+    path('delete_product/<int:id>/', delete_product, name = 'delete_product')
 ]
