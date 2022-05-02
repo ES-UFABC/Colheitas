4fa393a19d08cdbcaa81a684f589eb737a7f3527
Isaque Lopes Campello <isaque.campello@gmail.com>
2022-05-02 20:51:07 -0300

    added seller profile page

--- colheitas/products/views.py
+++ colheitas/products/views.py
@@ -9,15 +9,6 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from .models import Product
 from .forms import ProductRegisterForm
 
-def delete_product(request, id):
-    product_to_delete = Product.objects.get(id=id)
-    
-    if request.method == 'POST':
-        product_to_delete.delete()
-        return redirect('product_delete.html')
-    
-    return render(request, 'delete_confirm.html', {'product': product_to_delete})
-
 class ProductRegisterView(UserPassesTestMixin, generic.CreateView):
 
     def test_func(self):
@@ -46,10 +37,6 @@ class ProductsListSeller(generic.ListView):
         queryset = Product.objects.filter(seller=self.request.user.seller)
         return queryset
 
-def product_detail(request, id):
-    product = Product.objects.get(id=id)
-    return render(request, 'products/product_detail.html', {'product':product})
-   
 class ProductListState(generic.ListView):
     model = Product
     template_name = 'products/product_list_state.html'
@@ -57,3 +44,16 @@ class ProductListState(generic.ListView):
     def get_queryset(self):
         queryset = Product.objects.filter(state=self.request.user.state)
         return queryset
+
+def product_detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'products/product_detail.html', {'product':product})
+
+def delete_product(request, id):
+    product_to_delete = Product.objects.get(id=id)
+    
+    if request.method == 'POST':
+        product_to_delete.delete()
+        return redirect('product_delete.html')
+    
+    return render(request, 'delete_confirm.html', {'product': product_to_delete})
